28a4f8f2f012adb87afb78cda738c0ab404d9e1e
Dani Moreno <13841-12@aulescidet.local>
2020-03-02 14:44:33 +0100

    botiga

--- exercici52b.php
+++ exercici52b.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="estils.css">
 </head>
 <body>
     <?php
@@ -19,22 +20,22 @@
     $total=0;
     $productos=$_GET["item"];
     foreach ($productos as $indice=>$valor){
-           echo "has comprado $indice, que vale $valor <br>";
+           echo "<p style= 'color: white;'>has comprado $indice, que vale $valor <br>";
            $total=$total+$valor;
         }
-        echo "en total te has gastado $total <br>";
+        echo "<p style= 'color: white;'>en total te has gastado $total <br>";
         
    
          
     if ($total<=100){
-        echo "Su compra es demasiado pequeña , no obtiene ningun descuento<br>
+        echo "<p style= 'color: white;'>Su compra es demasiado pequeña , no obtiene ningun descuento<br>
         El precio le queda igual, serán $total";
     }else if ($total>100 and $total<=500){
-        echo "Su descuento es del 10% por haber gastado hasta $total euros<br>
+        echo "<p style= 'color: white;'>Su descuento es del 10% por haber gastado hasta $total euros<br>
         El precio le queda en: "; 
         echo $total-discount1($total); 
     }else{
-        echo "Obtiene un 15% de descuento por haber gastado hasta $total euros<br>
+        echo "<p style= 'color: white;'>Obtiene un 15% de descuento por haber gastado hasta $total euros<br>
         El precio le queda en: ";
         echo $total-discount2($total); 
     }
